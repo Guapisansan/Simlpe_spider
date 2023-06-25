44870e17f1e4e76cf4188c40ab74cd0608ba41bc
guapisansan <1776648377@qq.com>
2023-06-25 16:14:03 +0800

    fix: 数据库配置修复

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ config_Data = read_config_file()
 pool = redis.ConnectionPool(host=config_Data.get("redis").get("host"), db=15, password='')
 # 初始化数据库连接:
 mysql_config_dict = config_Data.get("mysql")
-SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{mysql_config_dict.get("user")}:{mysql_config_dict.get("password")}@{mysql_config_dict.get("host")}:{mysql_config_dict.get("port")}/mysql_config_dict.get("databases")?charset=utf8mb4&autocommit=true'
+SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{mysql_config_dict.get("user")}:{mysql_config_dict.get("password")}@{mysql_config_dict.get("host")}:{mysql_config_dict.get("port")}/{mysql_config_dict.get("databases")}?charset=utf8mb4&autocommit=true'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600, pool_size=0, max_overflow=1000, pool_pre_ping=True)
 # 创建DBSession类型:
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
